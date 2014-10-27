d95d3a35ffeb5998f23793779dc4d3e4d7c6d009
eliza-irene <elizabeth.korthof@gmail.com>
2014-10-27 16:11:07 -0400

    Finished creating static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
 root  'static_pages#home'
 
-match '/home', to: 'static_pages#home', via: 'get'
+match '/home', to: 'static_pages#home',   via: 'get'
 match '/about', to: 'static_pages#about', via: 'get'
-match '/help', to: 'static_pages#help', via: 'get'
 
 match '/signup', to: 'users#new',         via: 'get'
 match '/signin', to: 'sessions#new',      via: 'get'
