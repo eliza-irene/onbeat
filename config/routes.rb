4f9ef5ed2c147978ca5bb292cff85b0bc9e9bd7d
eliza-irene <elizabeth.korthof@gmail.com>
2014-10-27 14:18:26 -0400

    fixing merge errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
 root  'static_pages#home'
-match '/signup',  to: 'users#new',      via: 'get'
 
-
-# match '/signin', to: 'sessions#new', via: 'get'
-
+match '/signup', to: 'users#new',         via: 'get'
+match '/signin', to: 'sessions#new',      via: 'get'
+match '/signout', to: 'sessions#destroy', via: 'delete'
 resources :users
 # resources :sessions, only: [:create, :destroy]
 
